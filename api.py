f77d85954d6157a234b3fb86be6e16779f967af9
potasium2 <noahsaber64@aol.com>
2025-04-19 01:52:50 -0700

    Add todo
    a

--- api.py
+++ api.py
@@ -4,6 +4,15 @@ clientID = 36248
 clientSecret = 'kwQ1Jf9vvoRo3DY0pYiGYAsu3ETK0HSLhyNfEXRj'
 api = Ossapi(clientID, clientSecret)
 
+# TODO:
+# Loop through maps starting at like idk 2015-2017 or so
+# Check if beatmap is ranked, if not then skip it
+# If beatmap is ranked filter it based on the rules.txt
+# Save all relevant data (Song Title, Difficulty Name, setID, difficultyID, Star Rating) to an object list
+# Export that list as json data
+# I'll quick test on like 25-50 maps depending on slow this is (I also may switch to an async structure we'll see)
+
+# Starting Base:
 beatmapID = api.beatmapset(347136).beatmaps[3].id
 
 print(f"Aim Difficulty: {round(api.beatmap_attributes(beatmapID).attributes.aim_difficulty, 2)}")
