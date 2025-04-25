47ef317745e2be5dc13cf94571129a962d946b1a
potasium2 <noahsaber64@aol.com>
2025-04-25 16:17:30 -0700

    fixes
    Fixed stamina maps adding to speed array, made things a little faster

--- api.py
+++ api.py
@@ -4,8 +4,8 @@ import json
 # Client Stuff
 # If you are using this for personal use then create your own ID and Secret
 # You can do so here: https://osu.ppy.sh/home/account/edit#oauth
-clientID = 36248
-clientSecret = 'kwQ1Jf9vvoRo3DY0pYiGYAsu3ETK0HSLhyNfEXRj'
+clientID = 13497
+clientSecret = 'aiohgeoaihg93hghoiah09veioha0fvhj09a'
 api = Ossapi(clientID, clientSecret)
 
 def BeatMapFetcher(startingID, maximumID):
@@ -17,17 +17,21 @@ def BeatMapFetcher(startingID, maximumID):
         try:
             beatmapset = api.beatmapset(beatmapID)
         except:
-            print(f"Could not find beatmap at {beatmapID}, continuing to next beatmap.")
+            # print(f"Could not find beatmap at {beatmapID}, continuing to next beatmap.")
             continue
 
+        # Predetermine whether we want to calculate a set, this mostly saves a little bit of time
+        if beatmapset.beatmaps[0].mode != GameMode.OSU or beatmapset.beatmaps[0].ranked.value != 1:
+            continue
+        
         for beatmap in beatmapset.beatmaps:
             # I'll probably either implement other Game Mode functionality OR make them separate calculators
             if beatmap.mode != GameMode.OSU or beatmap.ranked.value != 1:
                 continue
 
             CalculateMapType(beatmapset, beatmap)
+            print(f"Beatmap {beatmap.id} of set {beatmapID} Returns good, continuing to next beatmap.")
 
-        print(f"Beatmap #{beatmapID} Returns good, continuing to next beatmap.")
 
 aimMaps = []
 speedMaps = []
@@ -49,24 +53,27 @@ def CalculateMapType(mapset, beatmap):
         "difficulty": api.beatmap_attributes(beatmap.id).attributes.star_rating
     }
 
-    if aimDifficulty > speedDifficulty + 0.5:
+    # Add maps to set based on rules
+    if aimDifficulty > speedDifficulty + 0.25:
         print(f"Aim Map: {beatmap.version}")
         aimMaps.append(beatMapObj)
     elif speedNoteCount > maxCombo * 0.2:
         print(f"Stamina Map: {beatmap.version}")
-        speedMaps.append(beatMapObj)
-    elif speedDifficulty > aimDifficulty + 0.5 and speedNoteCount > maxCombo * 0.1:
+        staminaMaps.append(beatMapObj)
+    elif speedDifficulty > aimDifficulty + 0.25 and speedNoteCount > maxCombo * 0.1:
         print(f"Speed Map: {beatmap.version}")
         speedMaps.append(beatMapObj)
-    elif abs(aimDifficulty - speedDifficulty) < 0.5 and speedNoteCount > maxCombo * 0.1:
-        print(f"Hybrid Map: {beatmap.version}")
-        hybridMaps.append(beatMapObj)
     else:
         print(f"Consistency Map: {beatmap.version}")
         consistencyMaps.append(beatMapObj)
+    
+    # Primarily Aim Maps can be Hybrid, Primarily Speed Maps can also be Hybrid
+    if abs(aimDifficulty - speedDifficulty) < 0.5 and speedNoteCount > maxCombo * 0.1:
+        print(f"Hybrid Map: {beatmap.version}")
+        hybridMaps.append(beatMapObj)
 
 # Starting in ~April 2015 -- Ending in ~April 2025
-BeatMapFetcher(300033, 2347113)
+BeatMapFetcher(300033, 400004) # 2347113
 allBeatMaps = {
     "aimMaps": aimMaps, 
     "speedMaps": speedMaps, 
